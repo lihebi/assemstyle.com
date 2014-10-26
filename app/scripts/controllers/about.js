8ff7f9f4f3c8b946ad4015ab0a4c7428d5807ba9
Hebi Li <hebi@iastate.edu>
2014-10-25 21:40:22 -0500

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assemstylecomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the assemstylecomApp
+ */
+angular.module('assemstylecomApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assemstylecomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the assemstylecomApp
+ */
+angular.module('assemstylecomApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
